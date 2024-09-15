f8159521daef134a874a1d0bff459b410e8981d3
jackyonderbrack <mike.projektowanie@gmail.com>
2024-09-15 22:07:43 +0200

    Debug: Poprawienie modelu użytkownika

--- server/src/utils/uuid.util.ts
+++ server/src/utils/uuid.util.ts
@@ -1,8 +1,8 @@
 export const uuid = (digits: number): string => {
-    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let id: string = '';
-    for (let i = 0; i < digits; i++) {
-        id += chars.charAt(Math.floor(Math.random() * chars.length))
-    }
-    return id
-} 
+	const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+	let id: string = "";
+	for (let i = 0; i < digits; i++) {
+		id += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+	return id;
+};
