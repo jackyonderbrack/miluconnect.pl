68a077d8740a00af0ddeffe50e7c457a2dfa6e76
jackyonderbrack <mike.projektowanie@gmail.com>
2024-10-20 13:10:45 +0200

    Fix: Unauthorized route to login

--- client/src/admin/components/PrivateRoute/PrivateRoute.tsx
+++ client/src/admin/components/PrivateRoute/PrivateRoute.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-const local = import.meta.env.DEV;
 
 const PrivateRoute: React.FC = () => {
 	const token = localStorage.getItem('TOKEN');
-
-	if (local) {
-		return <Outlet />;
-	} else {
-		return token ? <Outlet /> : <Navigate to='/admin-login' />;
-	}
+	
+	return token ? <Outlet /> : <Navigate to='/admin-login' />;
+	
 };
 
 export default PrivateRoute;
