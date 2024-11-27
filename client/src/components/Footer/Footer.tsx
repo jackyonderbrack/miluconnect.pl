247cec8c1fd8a0a29579d45fef2320c8a4cdfa45
Wiktoria Tokarska <tokarska.dev@gmail.com>
2024-11-27 16:24:56 +0100

    Icon change

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -43,22 +43,22 @@ const Footer = () => {
 								/>
 							</a>
 						</div>
-
-						{/* <div className="socialIconContainer">
-              <img
-                className="instagramSocialIcon"
-                src={InstagramSocial}
-                alt="Instagram social icon"
-              />
-              <img
-                className="instagramSocialIconHover"
-                src={InstagramSocialHover}
-                alt="Instagram social icon"
-              />
-            </div> */}
-
 						<div className='socialIconContainer'>
 							<a href='https://github.com/jackyonderbrack' target='_blank'>
+								<img
+									className='githubSocialIcon'
+									src={GithubSocial}
+									alt='Github social icon'
+								/>
+								<img
+									className='githubSocialIconHover'
+									src={GithubSocialHover}
+									alt='Github social icon'
+								/>
+							</a>
+						</div>
+						<div className='socialIconContainer'>
+							<a href='https://github.com/tokarskadev' target='_blank'>
 								<img
 									className='githubSocialIcon'
 									src={GithubSocial}
