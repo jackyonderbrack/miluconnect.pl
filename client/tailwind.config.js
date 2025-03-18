db66d00a9c1fa45659a0a453345de17f7498eda5
Michał Łuczak <mike.projektowanie@gmail.com>
2025-03-18 15:03:58 +0100

    Feature: TS upgrade and font static added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['DM Sans', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	base: './',
 	build: {
-		cssCodeSplit: false, 
+		cssCodeSplit: false,
+		assetsInlineLimit: 0,
 	},
-},);
+});
